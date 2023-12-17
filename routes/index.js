3f2f9733690d534d0319fdfd851c06626396e997
Juanan Pereira <juanan.pereira@ehu.eus>
2023-12-17 19:37:33 +0100

    Refactor user registration validation

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,7 @@ router.get('/', function(req, res, next) {
 router.post('/register', (req, res) => {
   const { username, email, password, confirm_password } = req.body;
 
-  // Server-side validation
-  if (!username || username.length < 5) {
-    return res.status(400).send('Username must be at least 5 characters long.');
-}
-
-  const passwordRegex = /^[a-zA-Z0-9]+$/;
-  if (!password || !password.match(passwordRegex) || password.length < 6) {
-      return res.status(400).send('Password must be alphanumeric and at least 6 characters long.');
-  }
-
-  if (password !== confirm_password) {
-      return res.status(400).send('Passwords do not match.');
-  }
+  
 
   // Echoing back the data (excluding password for security)
   res.json({ username, email });
